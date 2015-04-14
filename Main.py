38e7104dbde2536b3707677a03b427d5f18efa8f
Francis <francis-r-emond@hotmail.com>
2015-04-14 09:45:57 -0400

    against mlagent

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ from MLAgent import *
 from AI import *
 from FMAI import *
 
-ai1 = AI()
+ai1 = MLAgent()
 nbWin = 0
-for i in xrange(1):
+for i in xrange(100):
     ai2 = FMAI(0.5)
-    controller = Controller("Americas", "Basic", "FM", ai1, ai2)
+    controller = Controller("Americas", "Scrubby", "FM", ai1, ai2)
     winningPlayerIndex = controller.play()
     if winningPlayerIndex == 1:
         nbWin += 1
