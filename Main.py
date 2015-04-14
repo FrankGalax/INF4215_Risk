374d5183b70d512fa45f8743c03df4222ffefb82
Francis <francis-r-emond@hotmail.com>
2015-04-14 09:36:12 -0400

    include mlagent

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from Controller import *
+from MLAgent import *
 from AI import *
 from FMAI import *
 
 ai1 = AI()
-ai2 = FMAI(0.5)
 nbWin = 0
 for i in xrange(1):
+    ai2 = FMAI(0.5)
     controller = Controller("Americas", "Basic", "FM", ai1, ai2)
     winningPlayerIndex = controller.play()
     if winningPlayerIndex == 1:
